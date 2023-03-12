b2c712a55c26b348c6441298d86ac1a2796e3020
Luca Gugelmann <luca.gugelmann@gmail.com>
2023-03-12 18:09:47 +0100

    Move ps2 code to new serial module

--- asm/ps2_keyboard.s
+++ asm/ps2_keyboard.s
@@ -4,26 +4,20 @@
 
         org $e000
 
-        include include/registers
         include include/ps2_decoder
+        include include/serial
 
 start:
         lds #$0200              ; Initialize the stack
         jsr ps2_decoder_init
-
-        lda #TRMCR_CC0
-        sta TRMCR               ; Set clock source to internal and rate to E/16
-
-        lda #TRCSR_TE|TRCSR_RE  ; Enable read / transmit
-        sta TRCSR
-
+        jsr serial_init
         cli                     ; Enable interrupts
 
 loop:
         jsr ps2_decoder_get_key
         cmp B,#$01
         bne .control_code
-        jsr send_byte
+        jsr serial_send_byte
         bra loop                ; Sit around waiting for IRQs
 
 .control_code:
@@ -32,33 +26,22 @@ loop:
         ;; ASCII backspace just means 'move one back' on terminals. To implement
         ;; 'real' backspace we need to send backspace, space, backspace.
         lda #$08                ; ASCII backspace
-        jsr send_byte
+        jsr serial_send_byte
         lda #$20                ; ASCII space
-        jsr send_byte
+        jsr serial_send_byte
         lda #$08                ; ASCII backspace
-        jsr send_byte
+        jsr serial_send_byte
 
 +
         cmp A,#$5A              ; Enter
         bne +
         lda #$0D                ; CR
-        jsr send_byte
+        jsr serial_send_byte
         lda #$0A                ; LF
-        jsr send_byte
+        jsr serial_send_byte
 
 +
         bra loop
-
-        ;; Send byte stored in A
-send_byte:
-        ;; Wait for the send queue to be empty
-.wait_empty:
-        ldb TRCSR
-        bit B,#TRCSR_TDRE
-        beq .wait_empty
-
-        sta TDR
-        rts
 
 
 irq:
