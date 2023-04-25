70742cfb42fb8950c78b4c6bdcc25f60a06a1892
Luca Gugelmann <luca.gugelmann@gmail.com>
2023-04-25 18:12:10 +0200

    Make the monitor invokable from programs with a special key and add run/continue
    With this change the monitor is always invokable by pressing 'break'. Adds a
    'run' command that runs a test program and a 'continue' command that resumes
    execution of a program.

--- asm/monitor.s
+++ asm/monitor.s
@@ -2,17 +2,42 @@
 
         org $f000
 
-        include include/graphics
-        include include/keyboard
         include include/macros
         include include/registers
-        include include/serial
+        include include/stdio
 
-BUFFER_SIZE = GRAPHICS_TERMINAL_WIDTH
-        ; Input buffer, holds one line
-        reserve_memory buffer, BUFFER_SIZE
-        reserve_memory buffer_ptr, 2
-        reserve_memory command_ptr, 2
+INPUT_BUFFER_SIZE = GRAPHICS_TERMINAL_WIDTH
+USER_STACK_START = $7dff
+MONITOR_STACK_START = $7eff
+
+; Memory map:
+; 0000-001f  reserved, internal registers
+; 0020-7eff  RAM, of which
+;   20-00ff  Zero page addresses, program use
+;  100-7dff  Program memory, with stack at 7dff growing down
+; 7e00-7eff  Monitor memory, with stack at 7eff growing down
+; 7f00-7fff  I/O space, of which:
+;   00-  bf  [unused]
+;   c0-  ff  Graphics registers
+; 8000-ffff  ROM
+
+        orgsave
+        org $7e00
+
+input_buffer: ; Input buffer, holds one line
+        ds.b INPUT_BUFFER_SIZE
+input_buffer_ptr:
+        ds.b 2
+command_ptr:
+        ds.b 2
+user_stack_ptr:
+        ds.b 2
+
+        if * > $7eff - 64       ; Probably way more than needed
+          error "Not enough space for monitor stack left"
+        endif
+
+        orgrestore
 
 terminal_string:
         byt KEY_ENTER
@@ -33,6 +58,12 @@ bye_command:
 reset_command:
         byt "reset\0"
         adr start
+run_command:
+        byt "run\0"
+        adr run
+continue_command:
+        byt "continue\0"
+        adr continue
 COMMANDS_SIZE = * - commands
 
 
@@ -40,22 +71,21 @@ start:
         lda #RAMCR_RAM_ENABLE   ; Disable onboard RAM
         sta RAMCR
 
-        lds #$7eff              ; Set stack to top-of-RAM
+        lds #MONITOR_STACK_START ; Set stack to monitor stack location
 
-        jsr keyboard_init
-        jsr serial_init
-        jsr graphics_init
+        jsr stdio_init          ; Initializes serial, keyboard & graphics
 
         cli                     ; Enable interrupts
 
 line_start:
         ldx #terminal_string
-        jsr print
-        clr buffer
-        ldx #buffer
+        jsr putstring
+        ; Put a string-terminating 0 at the start of the input buffer
+        clr input_buffer
+        ldx #input_buffer
 
 keyboard_in:
-        jsr keyboard_getchar
+        jsr getchar
         cmp a,#0
         beq keyboard_in
 
@@ -66,30 +96,31 @@ keyboard_in:
         beq keyboard_in         ; Ignore delete for now, not supported
 
         cmp a,#KEY_ENTER
-        bne .not_exec
+        bne .not_enter
 
-        jsr graphics_putchar    ; Put enter on the screen
+        jsr putchar             ; Put enter on the screen
         jsr exec
         bra line_start
-.not_exec:
+
+.not_enter:
         ; Make sure not to overflow the input buffer
-        cpx #(buffer + BUFFER_SIZE - 2)
+        cpx #(input_buffer + INPUT_BUFFER_SIZE - 2)
         bgt line_start
 
         sta 0,x                 ; Store character in buffer
-        jsr graphics_putchar    ; Put it on the screen
+        jsr putchar             ; Put it on the screen
         inx
         clr 0,x                 ; Put a 0 at the new buffer end
         bra keyboard_in
 
 ; Figure out what command we intend to run and run it.
 exec:
-        ldx #buffer
-        stx buffer_ptr
+        ldx #input_buffer
+        stx input_buffer_ptr
         ldx #commands
         stx command_ptr
 .loop:
-        ldx buffer_ptr
+        ldx input_buffer_ptr
         lda 0,x
         ldx command_ptr
         ldb 0,x
@@ -97,22 +128,22 @@ exec:
         cba                     ; compare buffer and command bytes
         bne .next_command       ; mismatch, try the next command
 
-        cmp a,#0
+        tst a                   ; 0 or negative (but we have no negatives in A)
         beq .found_command      ; We reached the end of the buffer
                                 ; string. Match!
         inx
         stx command_ptr
-        ldx buffer_ptr
+        ldx input_buffer_ptr
         inx
-        stx buffer_ptr
+        stx input_buffer_ptr
         bra .loop
 
 .next_command:
-        cmp b,#0
+        tst b                   ; 0 or negative but no negatives in B
         bne .next_loop
         ; We found a 0 in the commands list, we're at the end of a command
         ; string. Next are 2 bytes of addresses to skip, and we need to reset
-        ; the buffer pointer.
+        ; the input buffer pointer.
         inx
         inx
         inx
@@ -121,8 +152,8 @@ exec:
         cpx #(commands + COMMANDS_SIZE)
         bge .error
 
-        ldx #buffer
-        stx buffer_ptr
+        ldx #input_buffer
+        stx input_buffer_ptr
         bra .loop
 
 .next_loop:
@@ -140,39 +171,78 @@ exec:
 
 .error:
         ldx #error_string
-        bsr print
-        ldx #buffer
-        bsr print
+        jsr putstring
+        ldx #input_buffer
+        jsr putstring
         rts
-
-; Print the 0-terminated string pointed to by X. Clobbers A, X.
-print:
-        lda 0,x
-        beq .end
-        inx
-        jsr graphics_putchar
-        bra print
-.end:
-        rts
-
 
 hello_string:
         byt "Hello World!\0"
 hello:
         ldx #hello_string
-        bsr print
+        jsr putstring
         rts
 
 bye_string:
         byt "Bye\0"
 bye:
         ldx #bye_string
-        bsr print
+        jsr putstring
         rts
 
-irq:
-        jsr keyboard_irq
+run:
+        lds #USER_STACK_START
+        ; Push the monitor start address on the user stack so a program can rts
+        ; back to it.
+        ldx #line_start
+        pshx
+        jmp test_user_program
+
+continue:
+        lds user_stack_ptr
+
+        ; The user program was interrupted and still has program counter,
+        ; accumulators, etc on the user stack. RTI restores these and continues
+        ; execution.
         rti
+
+irq:
+        ; The 6301 puts stack pointer, accumulators, register flags etc on the
+        ; stack before entering an interrupt routine. We don't need to take
+        ; precautions to preserve them.
+        jsr keyboard_irq
+        bvs .invoke_monitor     ; keyboard IRQ sets V if 'break' key is pressed
+        rti
+
+.invoke_monitor:
+        sts user_stack_ptr      ; Store the current user stack pointer
+        lds #MONITOR_STACK_START
+        cli                     ; Re-enable interrupts, they are disabled when
+                                ; an interrupt is called
+        jmp line_start
+
+
+; A small test program that just echos the typed characters back on the screen
+; or exits with 'X' (capital X). Prints a string when freshly started to help
+; distinguish 'run' from 'continue'.
+test_string:
+        byt "\nUser program start\n\0"
+test_user_program:
+        clr GRAPHICS_CLEAR      ; Clear screen
+        ldx #test_string
+        jsr putstring
+        jsr test_user_program_function
+        rts                     ; Back to the monitor
+
+test_user_program_function:
+        jsr getchar
+        tst a
+        beq test_user_program_function
+        jsr putchar
+
+        cmp a,#"X"
+        bne test_user_program_function
+        rts
 
         org $fff0
 vectors:
