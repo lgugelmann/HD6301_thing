80d626b04e1383dea7ac52c570d13454606fa785
Luca Gugelmann <luca.gugelmann@gmail.com>
2023-07-14 01:13:58 +0200

    ASM: make the sequencer program multi-track

--- asm/programs/seq.s
+++ asm/programs/seq.s
@@ -14,21 +14,35 @@
         SECTION seq
         PUBLIC seq_start
 
-        reserve_memory notes, 100
-        zp_var cursor_column, 1
-        zp_var playing, 1
-        zp_var play_pos, 2
-
+NOTE_LENGTH = 98                ; How many characters a 'notes' line is
 LAST_NOTE = 93                  ; Position of the first char of the last note
 TICK_INTERVAL = 4
+CHANNELS = 4
+PLAY_CURSOR_ROW = CHANNELS
+
+        reserve_memory notes, NOTE_LENGTH*CHANNELS
+        zp_var play_pos, 1      ; Offset into 'notes' for the next note
+        zp_var cursor_row, 1    ; Edit cursor row position
+        zp_var cursor_column, 1 ; Edit cursor column position
+        zp_var playing, 1       ; 0 not playing, 1 playing
 
 seq_start:
         clr GRAPHICS_CLEAR
         clr playing
+        clr cursor_row
         clr cursor_column
 
         jsr init_instrument
+
+        ldx #notes
+        lda #CHANNELS
+        ldb #NOTE_LENGTH
+.loop:
         jsr init_notes
+        abx                     ; move to next 'notes' line
+        dec a
+        bne .loop
+
         jsr edit_loop
         rts
 
@@ -37,10 +51,29 @@ init_instrument:
         ldx #instruments_piano
         jsr sound_load_instrument
         jsr sound_stop_note
+
+        lda #2                  ; Channel number
+        ldx #instruments_piano
+        jsr sound_load_instrument
+        jsr sound_stop_note
+
+        lda #3                  ; Channel number
+        ldx #instruments_piano
+        jsr sound_load_instrument
+        jsr sound_stop_note
+
+        lda #4                  ; Channel number
+        ldx #instruments_tsch
+        jsr sound_load_instrument
+        jsr sound_stop_note
         rts
 
+; Init the notes buffer in X
 init_notes:
-        ldx #notes
+        pshx
+        psh a
+        psh b
+
         lda #6
 
         inx
@@ -72,25 +105,40 @@ init_notes:
         sta 0,x
         clr 1,x
 
+        pul b
+        pul a
+        pulx
         rts
 
 ; Redraw the notes string on the screen
 refresh_notes:
         pshx
-        psh a
 
-        clr GRAPHICS_SET_COLUMN
+        clr a
         ldx #notes
+.loop:
+        clr b
+        stb GRAPHICS_SET_COLUMN
+        sta GRAPHICS_SET_ROW
 
-        lda #1
-        sta GRAPHICS_HIDE_CURSOR ; Cursor off to make putstring look better
-        jsr putstring
+        ldb #1
+        stb GRAPHICS_HIDE_CURSOR ; Cursor off to make putstring look better
+        jsr putstring            ; X now points to the 0 at the end of 'notesX'
         clr GRAPHICS_HIDE_CURSOR ; Cursor back on
 
+        ; Set X to the start of the next 'notes' string
+        inx
+
+        inc a
+        cmp a,#CHANNELS
+        bne .loop
+
+        ; Set cursor to the edit row / column
+        lda cursor_row
+        sta GRAPHICS_SET_ROW
         lda cursor_column
         sta GRAPHICS_SET_COLUMN
 
-        pul a
         pulx
         rts
 
@@ -140,8 +188,6 @@ edit_loop:
         jsr getchar
         beq .charloop
 
-        jsr stop
-
         cmp a,#'x'
         bne +
         sta 0,x
@@ -190,34 +236,67 @@ edit_loop:
 +
         cmp a,#KEY_RIGHT
         bne +
-        cpx #notes+LAST_NOTE    ; Are we all the way at the right?
+        lda cursor_column
+        cmp a,#LAST_NOTE        ; Are we all the way at the right?
         beq .next               ; yes, ignore
         ldb #4
         abx
-        add b,cursor_column
-        stb cursor_column
+        aba
+        sta cursor_column
         jmp .next
 +
         cmp a,#KEY_LEFT
         bne +
-        cpx #notes+1            ; Are we all the way at the left?
+        ldb cursor_column
+        cmp b,#1                ; Are we all the way at the left?
         beq .next2              ; if yes, ignore
         dex
         dex
         dex
         dex
-        ldb cursor_column
         sub b,#4
         stb cursor_column
         jmp .next
 +
+        cmp a,#KEY_UP
+        bne +
+        lda cursor_row
+        beq .next2              ; If 0, nothing to do
+        dec a
+        sta cursor_row
+        ; We don't have a way to decrement X easily, so we need to sum up
+        ldx #notes
+        ldb cursor_column
+        abx
+        ldb #NOTE_LENGTH
+.key_up_add_loop:
+        tst a
+        beq .next2
+        abx
+        dec a
+        bra .key_up_add_loop
++
+        cmp a,#KEY_DOWN
+        bne +
+        lda cursor_row
+        cmp a,#CHANNELS-1
+        beq .next2              ; If at the last row already, nothing to do
+        inc a
+        sta cursor_row
+        ldb #NOTE_LENGTH
+        abx
+        jmp .next
++
         cmp a,#' '
         bne .next2
-        ldd #notes+1            ; Reset playing position
-        std play_pos
+        lda #1                  ; Reset playing position
+        sta play_pos
         clr timer_ticks
-        inc playing             ; playing is 0, set it to 1
+        eim #1,playing          ; Start / stop playing
+        beq .stop_playing       ; If we're stopping, stop
         jmp .charloop
+.stop_playing:
+        jsr stop
 .next2:
         jmp .next
         rts
@@ -226,15 +305,66 @@ edit_loop:
 ; and increase play_pos (or reset to the start).
 play_next:
         pshx
-        ldx play_pos
 
+        ; Show a caret under the note played
+        lda #PLAY_CURSOR_ROW
+        sta GRAPHICS_SET_ROW
+        ldb #1
+        stb GRAPHICS_CLEAR
+
+        ; Turns out that the graphics clear is really expensive and we lose the
+        ; next row/column operations if we don't nop for a bit.
+        nop10
+        nop10
+
+        sta GRAPHICS_SET_ROW
+        ldb play_pos
+        stb GRAPHICS_SET_COLUMN
+
+        lda #'^'
+        sta GRAPHICS_SEND_CHAR_RAW
+
+        ; Set the cursor back to the editing position
+        lda cursor_column
+        sta GRAPHICS_SET_COLUMN
+        lda cursor_row
+        sta GRAPHICS_SET_ROW
+
+        ; Set up X to point to the next note
+        ldx #notes
+        abx                     ; B is still play_pos
+        lda #1                  ; First channel number
+.channel_loop:
+        jsr play_next_channel
+
+        ldb #NOTE_LENGTH
+        abx                     ; Move to the next notes string
+
+        inc a
+        cmp a,#CHANNELS+1       ; +1 because channels in OPL3 are 1-based
+        bne .channel_loop
+
+        ldb play_pos
+        add b,#4
+        cmp b,#LAST_NOTE
+        ble .end2               ; branch if not past the end
+        ldb #1
+.end2:
+        stb play_pos
+
+        pulx
+        rts
+
+; Play the note X points to on the channel in A
+play_next_channel:
+        psh a
         lda 0,x
         cmp a,#'x'
         beq .end
 
         ; Octave to MIDI note number: C1 -> 24, C2 -> 36, so (octave#+1)*12
         lda 2,x                 ; octave number as ASCII digit
-        sub a,#'0'-1            ; sub '0'-1 to get the +1 we need
+        sub a,#'0'-1            ; sub '0'-1 to get the +1 in (octave#1+1)
         ldb #12
         mul                     ; low order byte (what we need) ends up in B
 
@@ -270,7 +400,7 @@ play_next:
         lda #11
         bra .sharp
 +
-        lda #0
+        clr a
 
 .sharp:
         aba                     ; Finish adding the note delta from above
@@ -288,27 +418,37 @@ play_next:
         inc b
 
 .play:
-        tba
-
-        lda #1                  ; Channel number, B contains the MIDI note
+        pul a                  ; Channel number, B contains the MIDI note
         jsr sound_play_note
+        bra .end2
 
 .end:
-        ldb #4
-        abx
-        cpx #notes+LAST_NOTE
-        ble .end2               ; branch if not past the end
-        ldx #notes+1
+        pul a
 .end2:
-        stx play_pos
-        pulx
         rts
 
 stop:
-        clr playing
         psh a
-        lda #1                  ; Channel number
+        lda #CHANNELS
+.loop:
         jsr sound_stop_note
+        dec a
+        bne .loop
+
+        ; Clear the row with the play caret
+        lda #PLAY_CURSOR_ROW
+        sta GRAPHICS_SET_ROW
+        lda #1
+        sta GRAPHICS_CLEAR      ; 1: clear row, reset cursor column to start
+
+        nop10
+        nop10
+
+        ; Reset cursor to edit row
+        lda cursor_row
+        sta GRAPHICS_SET_ROW
+        lda cursor_column
+        sta GRAPHICS_SET_COLUMN
         pul a
         rts
 
