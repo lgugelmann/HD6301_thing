a266198f5f023ee4977f28f1328132543db37e06
Luca Gugelmann <luca.gugelmann@gmail.com>
2023-07-16 19:13:42 +0200

    Graphics test: update for 'advance cursor' logic & add a color reference

--- asm/programs/graphics_test.s
+++ asm/programs/graphics_test.s
@@ -6,12 +6,16 @@
         PUBLIC graphics_test_start
 
 graphics_test_start:
+        clr GRAPHICS_CLEAR
+        nop10
+        nop10
         lda #1
         sta GRAPHICS_HIDE_CURSOR
         jsr init_strings
         jsr fg_color_test
         jsr bg_color_test
-        jsr both_color_test
+        jsr advance_color_test
+        jsr print_color_reference
         rts
 
 ; Write out a few lines of 'abcd..' text
@@ -27,7 +31,7 @@ init_strings:
         cmp a,#'z'+1
         bne .charloop
         inc b
-        cmp b,#4
+        cmp b,#5
         bne .rowloop
         rts
 
@@ -58,18 +62,46 @@ bg_color_test:
         bne .loop
         rts
 
-both_color_test:
+advance_color_test:
         ldb #3
         stb GRAPHICS_SET_ROW
         clr GRAPHICS_SET_COLUMN
-        ldb #1
-        lda #$80
+        lda #COLOR_ADVANCE
 .loop:
         sta GRAPHICS_SET_COLOR
-        stb GRAPHICS_MOVE_CURSOR
         inc a
-        cmp a,#$ff
+        cmp a,#$40 | COLOR_ADVANCE
         bne .loop
+        rts
+
+print_color_reference:
+        lda #KEY_ENTER
+        jsr putchar             ; After this the cursor is on a new row
+
+        ldb #0
+        ldx #16
+.loop:
+        tba
+        jsr putchar_hex
+
+        lda #-2
+        sta GRAPHICS_MOVE_CURSOR
+        eor b,#COLOR_BG | COLOR_ADVANCE
+        stb GRAPHICS_SET_COLOR
+        stb GRAPHICS_SET_COLOR
+        stb GRAPHICS_SET_COLOR
+        eor b,#COLOR_BG | COLOR_ADVANCE
+
+        inc b
+        dex
+        bne .loop
+        lda #KEY_ENTER
+        jsr putchar
+
+        ldx #16
+        cmp b,#$40
+        bne .loop
+
         rts
 
         ENDSECTION
