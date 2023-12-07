41adff30e651bd3a027f036cf3f734d1eacc7ca1
Luca Gugelmann <luca.gugelmann@gmail.com>
2023-12-07 22:29:59 +0100

    asm: rework sci_echo demo program to use stdlib

--- asm/programs/sci_echo.s
+++ asm/programs/sci_echo.s
@@ -2,47 +2,15 @@
 
         cpu 6301
 
+        include ../stdlib.inc
+
         SECTION sci_echo
         PUBLIC sci_echo_start
 
-TRMCR = $0010                   ; Transfer Rate / Mode Control Register
-TRCSR = $0011                   ; Transmit / Receive Control Status Register
-RDR   = $0012                   ; Receive Data Register
-TDR   = $0013                   ; Transmit Data Register
-
-
-TRMCR_CC0  = %00000100          ; CC0 bit (low bit of clock source settings)
-
-TRCSR_TE   = %00000010          ; Transmit Enable
-TRCSR_RE   = %00001000          ; Read Enable
-TRCSR_TDRE = %00100000          ; Transmit Data Register Empty
-TRCSR_RDRF = %10000000          ; Receive Data Register Full
-
-
 sci_echo_start:
-        lda #TRMCR_CC0
-        sta TRMCR               ; Set clock source to internal and rate to E/16
-
-        lda #TRCSR_TE|TRCSR_RE  ; Enable read / transmit
-        sta TRCSR
-
-wait_read:
-        lda TRCSR
-        bit A,#TRCSR_RDRF       ; receive register full -> nonzero result here
-        beq wait_read
-        lda RDR
-        jsr send_byte
-        bra wait_read
-
-        ;; Send byte stored in A
-send_byte:
-        ;; Wait for the send queue to be empty
-.wait_empty:
-        ldb TRCSR
-        bit B,#TRCSR_TDRE
-        beq .wait_empty
-
-        sta TDR
+        jsr serial_read_byte_blocking
+        jsr serial_send_byte
+        bra sci_echo_start
         rts
 
         ENDSECTION
