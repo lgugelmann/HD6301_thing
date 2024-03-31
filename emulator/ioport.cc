f2588885ddd486138c73f7650f0ba13996ebe56b
Luca Gugelmann <luca.gugelmann@gmail.com>
2024-03-31 16:18:39 +0200

    Emulator: Add IOPort class

---
+++ emulator/ioport.cc
@@ -0,0 +1,51 @@
+#include "ioport.h"
+
+#include <absl/log/log.h>
+#include <absl/strings/str_cat.h>
+
+#include <cstdio>
+#include <string>
+#include <string_view>
+
+namespace eight_bit {
+
+IOPort::IOPort(std::string_view name) : name_(name) {}
+
+void IOPort::write(uint8_t data) {
+  data_ = data;
+  VLOG(3) << "Write to " << name_ << ": "
+          << absl::Hex(data & data_direction_, absl::kZeroPad2);
+  for (const auto& callback : write_callbacks_) {
+    callback(data & data_direction_);
+  }
+}
+
+uint8_t IOPort::read() {
+  uint8_t read_data = 0;
+  for (const auto& callback : read_callbacks_) {
+    read_data |= callback();
+  }
+  // Bits set as outputs are returned as the last written data.
+  read_data |= data_ & data_direction_;
+  VLOG(3) << "Read from " << name_ << ": "
+          << absl::Hex(read_data, absl::kZeroPad2);
+  return read_data;
+}
+
+void IOPort::set_direction(uint8_t direction_mask) {
+  VLOG(1) << "Setting direction for " << name_ << " to "
+          << absl::Hex(direction_mask, absl::kZeroPad2);
+  data_direction_ = direction_mask;
+}
+
+uint8_t IOPort::get_direction() { return data_direction_; }
+
+void IOPort::register_read_callback(read_callback callback) {
+  read_callbacks_.push_back(callback);
+}
+
+void IOPort::register_write_callback(write_callback callback) {
+  write_callbacks_.push_back(callback);
+}
+
+}  // namespace eight_bit
---
+++ emulator/ioport.h
@@ -0,0 +1,57 @@
+#ifndef EIGHT_BIT_PORT_H
+#define EIGHT_BIT_PORT_H
+
+#include <cstdint>
+#include <functional>
+#include <string>
+
+namespace eight_bit {
+
+class IOPort {
+ public:
+  typedef std::function<uint8_t()> read_callback;
+  typedef std::function<void(uint8_t)> write_callback;
+
+  IOPort(std::string_view name);
+  ~IOPort() = default;
+
+  // Reads from the port. Note: assumes that the read callbacks do not return
+  // overlapping data and that they leave bits that aren't part of the callback
+  // device as 0.
+  uint8_t read();
+
+  // Writes to this port. Calls all write callbacks with the written data. Bits
+  // set as inputs are passed to the callbacks as zero.
+  void write(uint8_t data);
+
+  // Set the data direction for the port. Bits set to 0 are inputs, bits set to
+  // 1 are outputs.
+  void set_direction(uint8_t direction_mask);
+
+  // Get the data direction for the port.
+  uint8_t get_direction();
+
+  // Registers a callback that's called when read() is invoked on this port. The
+  // device at the other end is expected to provide data.
+  void register_read_callback(read_callback callback);
+
+  // Registers a write callback for this port.
+  void register_write_callback(write_callback callback);
+
+ private:
+  std::string name_;
+  // Callbacks for reading and writing to the port.
+  std::vector<read_callback> read_callbacks_;
+  std::vector<write_callback> write_callbacks_;
+
+  // Bits set to 0 are inputs, bits set to 1 are outputs.
+  uint8_t data_direction_ = 0;
+
+  // The data last written to the port. This is returned for output bits on
+  // reads.
+  uint8_t data_ = 0;
+};
+
+}  // namespace eight_bit
+
+#endif  // EIGHT_BIT_PORT_H
