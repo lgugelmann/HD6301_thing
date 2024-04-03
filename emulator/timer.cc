e07c59be02bcd45832b39a49cfef7027c67a318d
Luca Gugelmann <luca.gugelmann@gmail.com>
2024-04-03 22:31:08 +0200

    Emulator: add Timer class to handle timer functionality

---
+++ emulator/timer.cc
@@ -0,0 +1,109 @@
+#include "timer.h"
+
+#include <absl/log/log.h>
+#include <absl/strings/str_format.h>
+
+#include <cstdint>
+
+namespace eight_bit {
+namespace {
+constexpr uint8_t kTimerOverflow = 0x10;
+constexpr uint8_t kTimerInterruptEnable = 0x04;
+}  // namespace
+
+Timer::Timer(AddressSpace* address_space, u_int16_t base_address,
+             Interrupt* interrupt)
+    : address_space_(address_space),
+      base_address_(base_address),
+      interrupt_(interrupt) {
+  address_space_->register_read(0x0008, 0x0008, [this](uint16_t address) {
+    return read_status_register();
+  });
+  address_space_->register_write(
+      0x0008, 0x0008,
+      [this](uint16_t address, uint8_t data) { write_status_register(data); });
+  address_space_->register_read(
+      0x0009, 0x0009, [this](uint16_t address) { return read_counter_high(); });
+  address_space_->register_write(
+      0x0009, 0x0009,
+      [this](uint16_t address, uint8_t data) { write_counter_high(data); });
+  address_space_->register_read(
+      0x000a, 0x000a, [this](uint16_t address) { return read_counter_low(); });
+  address_space_->register_write(
+      0x000a, 0x000a,
+      [this](uint16_t address, uint8_t data) { write_counter_low(data); });
+}
+
+void Timer::tick() {
+  counter_ += 1;
+  if (counter_ == 0) {
+    VLOG(3) << "Timer overflow";
+    // Counter just overflowed. Set overflow bit, and fire an interrupt if
+    // enabled.
+    status_register_ |= kTimerOverflow;
+    if (status_register_ & kTimerInterruptEnable) {
+      if (interrupt_id_ == 0) {
+        VLOG(3) << "Timer interrupt";
+        interrupt_id_ = interrupt_->set_interrupt();
+      } else {
+        VLOG(3) << "Timer interrupt already pending";
+      }
+    }
+  }
+}
+
+uint8_t Timer::read_status_register() {
+  // If the overflow bit is set, reading the status register and then the
+  // counter will clear it.
+  if (status_register_ & kTimerOverflow) {
+    counter_read_clears_interrupt_ = true;
+  };
+  return status_register_;
+}
+
+void Timer::write_status_register(uint8_t value) {
+  // The top 3 bits are read-only.
+  status_register_ = value & 0x1f;
+  VLOG(1) << absl::StreamFormat("Timer status register set to %02x",
+                                status_register_);
+}
+
+uint8_t Timer::read_counter_low() {
+  if (counter_low_latched_) {
+    counter_low_latched_ = false;
+    return counter_low_latch_;
+  }
+  return counter_;
+}
+
+void Timer::write_counter_low(uint8_t data) {
+  if (counter_high_latched_) {
+    counter_high_latched_ = false;
+    counter_ = counter_high_latch_ << 8 | data;
+  } else {
+    counter_ = (counter_ & 0xff00) | (uint16_t)data;
+  }
+}
+
+uint8_t Timer::read_counter_high() {
+  uint16_t counter = counter_;
+  if (counter_read_clears_interrupt_) {
+    counter_read_clears_interrupt_ = false;
+    status_register_ &= ~kTimerOverflow;
+    if (interrupt_id_ != 0) {
+      interrupt_->clear_interrupt(interrupt_id_);
+      interrupt_id_ = 0;
+    }
+  }
+  counter_low_latch_ = counter;
+  counter_low_latched_ = true;
+  return counter >> 8;
+}
+
+void Timer::write_counter_high(uint8_t value) {
+  counter_high_latch_ = value;
+  counter_high_latched_ = true;
+  counter_ = 0xfff8;
+}
+
+}  // namespace eight_bit
---
+++ emulator/timer.h
@@ -0,0 +1,65 @@
+#ifndef EIGHT_BIT_TIMER_H
+#define EIGHT_BIT_TIMER_H
+
+#include <cstdint>
+
+#include "address_space.h"
+#include "interrupt.h"
+
+namespace eight_bit {
+
+class Timer {
+ public:
+  Timer(AddressSpace* address_space, u_int16_t base_address,
+        Interrupt* interrupt);
+  Timer(const Timer&) = delete;
+  Timer& operator=(const Timer&) = delete;
+  ~Timer() = default;
+
+  // Increment the counter.
+  void tick();
+
+  // Read/write access for the status register. The top 3 bits of the status
+  // register are read-only and are never changed on writes.
+  uint8_t read_status_register();
+  void write_status_register(uint8_t value);
+
+  // Read/write access for counter bytes. See the comments below for the
+  // latching and interrupt clearing logic.
+  uint8_t read_counter_low();
+  void write_counter_low(uint8_t value);
+  uint8_t read_counter_high();
+  void write_counter_high(uint8_t value);
+
+ private:
+  AddressSpace* address_space_ = nullptr;
+  const uint16_t base_address_ = 0;
+  Interrupt* interrupt_ = nullptr;
+
+  uint8_t status_register_ = 0;
+
+  // After reading the status register with the interrupt bit set, a subsequent
+  // read of the counter high byte will clear the interrupt bit.
+  bool counter_read_clears_interrupt_ = false;
+
+  // Reads to the high byte cause the low byte to be latched. The next read
+  // returns the latched value.
+  bool counter_low_latched_ = false;
+  uint8_t counter_low_latch_ = 0;
+
+  // When the high byte is written, the counter is reset to 0xfff8 regardless of
+  // the value written. When the low byte is written to next, the actual latched
+  // value is written to the counter.
+  bool counter_high_latched_ = false;
+  uint8_t counter_high_latch_ = 0;
+
+  // Tick counter
+  uint16_t counter_ = 0;
+
+  // The interrupt ID of the pending interrupt, or 0 if no interrupt is pending.
+  int interrupt_id_ = 0;
+};
+
+}  // namespace eight_bit
+
+#endif  // EIGHT_BIT_TIMER_H
