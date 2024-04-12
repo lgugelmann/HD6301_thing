b845f9c905b21f19f03baae0d3c921a890cc9a6f
Luca Gugelmann <luca.gugelmann@gmail.com>
2024-04-12 16:30:25 +0200

    Refactor Interrupt class to make it thread-safe

--- emulator/interrupt.h
+++ emulator/interrupt.h
@@ -1,10 +1,14 @@
 #ifndef EIGHT_BIT_COMPUTER_INTERRUPT_H
 #define EIGHT_BIT_COMPUTER_INTERRUPT_H
 
+#include <absl/synchronization/mutex.h>
+
 #include <set>
 
 namespace eight_bit {
 
+// A simple interrupt class that can be used to set and clear interrupts. Thread
+// safe.
 class Interrupt {
  public:
   Interrupt() = default;
@@ -13,6 +17,7 @@ class Interrupt {
   // Set an interrupt. The returned integer needs to be used to clear the
   // interrupt again.
   int set_interrupt() {
+    absl::MutexLock lock(&mutex_);
     int interrupt_id = next_interrupt_id_;
     // Technically speaking this could wrap around while something is still
     // active. We're going to ignore it and just say that maybe a few billion
@@ -26,17 +31,24 @@ class Interrupt {
   }
 
   // Clear the interrupt `interrupt`.
-  void clear_interrupt(int interrupt) { interrupts_.erase(interrupt); }
+  void clear_interrupt(int interrupt) {
+    absl::MutexLock lock(&mutex_);
+    interrupts_.erase(interrupt);
+  }
 
   // Returns true if this interrupt is firing (i.e. has outstanding interrupts),
   // false otherwise.
-  bool has_interrupt() { return interrupts_.size() > 0; }
+  bool has_interrupt() {
+    absl::MutexLock lock(&mutex_);
+    return interrupts_.size() > 0;
+  }
 
  private:
   // The next interrupt ID to use. Value 0 is never a valid interrupt ID and can
   // be used as a sentinel for 'no interrupt set'.
-  int next_interrupt_id_ = 1;
-  std::set<int> interrupts_;
+  absl::Mutex mutex_;
+  int next_interrupt_id_ = 1 ABSL_GUARDED_BY(mutex_);
+  std::set<int> interrupts_ ABSL_GUARDED_BY(mutex_);
 };
 
 }  // namespace eight_bit
