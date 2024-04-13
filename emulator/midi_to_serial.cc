7b4764b8c39a3746b399ca4d29f681d5447fe510
Luca Gugelmann <luca.gugelmann@gmail.com>
2024-04-14 00:53:18 +0200

    Emulator: add MidiToSerial class for forwarding MIDI bytes to a PTY

---
+++ emulator/midi_to_serial.cc
@@ -0,0 +1,52 @@
+#include "midi_to_serial.h"
+
+#include <absl/status/status.h>
+#include <absl/status/statusor.h>
+#include <absl/strings/str_cat.h>
+#include <fcntl.h>
+#include <rtmidi/RtMidi.h>
+#include <unistd.h>
+
+namespace eight_bit {
+
+MidiToSerial::~MidiToSerial() {
+  if (fd_ != -1) {
+    close(fd_);
+  }
+}
+
+absl::StatusOr<std::unique_ptr<MidiToSerial>> eight_bit::MidiToSerial::create(
+    std::string_view pty_name) {
+  std::unique_ptr<MidiToSerial> midi_to_serial(new MidiToSerial());
+  auto status = midi_to_serial->initialize(pty_name);
+  if (!status.ok()) {
+    return status;
+  }
+  return midi_to_serial;
+}
+
+absl::Status eight_bit::MidiToSerial::initialize(std::string_view pty_name) {
+  fd_ = open(pty_name.data(), O_WRONLY | O_NOCTTY);
+  if (fd_ == -1) {
+    return absl::InternalError(
+        absl::StrCat("Failed to open pty ", pty_name, " for writing"));
+  }
+
+  try {
+    midi_in_ = std::make_unique<RtMidiIn>(RtMidi::UNSPECIFIED, "Emulator MIDI");
+    midi_in_->openVirtualPort("Emulator MIDI in");
+    midi_in_->setCallback(
+        [](double /* timestamp */, std::vector<unsigned char>* message,
+           void* user_data) {
+          int fd = *static_cast<int*>(user_data);
+          ::write(fd, message->data(), message->size());
+        },
+        &fd_);
+  } catch (RtMidiError& error) {
+    return absl::InternalError(
+        absl::StrCat("Failed to open virtual MIDI port: ", error.what()));
+  }
+  return absl::OkStatus();
+}
+
+}  // namespace eight_bit
---
+++ emulator/midi_to_serial.h
@@ -0,0 +1,35 @@
+#ifndef EIGHT_BIT_MIDI_TO_SERIAL_H
+#define EIGHT_BIT_MIDI_TO_SERIAL_H
+
+#include <absl/status/statusor.h>
+#include <rtmidi/RtMidi.h>
+
+#include <memory>
+#include <string_view>
+
+namespace eight_bit {
+
+// A class that creates a virtual MIDI port and forwards incoming MIDI bytes to
+// a PTY
+class MidiToSerial {
+ public:
+  MidiToSerial(const MidiToSerial&) = delete;
+  MidiToSerial& operator=(const MidiToSerial&) = delete;
+  ~MidiToSerial();
+
+  // pty_name is the name of the pseudo-terminal to write to.
+  static absl::StatusOr<std::unique_ptr<MidiToSerial>> create(
+      std::string_view pty_name);
+
+ private:
+  MidiToSerial() = default;
+
+  absl::Status initialize(std::string_view pty_name);
+
+  int fd_ = -1;
+  std::unique_ptr<RtMidiIn> midi_in_;
+};
+
+}  // namespace eight_bit
+
+#endif  // EIGHT_BIT_MIDI_TO_SERIAL_H
