98a7f5fc6e1094d6227f932c52861a6aac744c55
Luca Gugelmann <luca.gugelmann@gmail.com>
2024-05-03 22:40:50 +0200

    SD Emulation: add block read/write support and the ability to load images from file

--- emulator/sd_card_spi.cc
+++ emulator/sd_card_spi.cc
@@ -1,5 +1,13 @@
 #include "sd_card_spi.h"
 
+#include <fstream>
+#include <istream>
+#include <memory>
+#include <spanstream>
+#include <string>
+#include <string_view>
+#include <vector>
+
 #include "absl/log/log.h"
 #include "absl/status/status.h"
 #include "absl/status/statusor.h"
@@ -8,18 +16,79 @@
 
 namespace eight_bit {
 namespace {
+//
+// R1 bits
+//
 uint8_t kR1InIdleState = 0x01;
 // uint8_t kR1EraseReset = 0x02;
 uint8_t kR1IllegalCommand = 0x04;
 uint8_t kR1CommandCRCError = 0x08;
 // uint8_t kR1EraseSequenceError = 0x10;
 // uint8_t kR1AddressError = 0x20;
-// uint8_t kR1ParameterError = 0x40;
+uint8_t kR1ParameterError = 0x40;
+
+//
+// Data Error Token bits
+//
+uint8_t kDataErrorTokenError = 0x01;
+// uint8_t kDataErrorTokenCCError = 0x02;
+// uint8_t kDataErrorTokenECCFailed = 0x04;
+// uint8_t kDataErrorTokenOutOfRange = 0x08;
+
+constexpr int kBlockSize = 512;
 }  // namespace
 
 absl::StatusOr<std::unique_ptr<SDCardSPI>> SDCardSPI::create(SPI* spi) {
   auto sd_card_spi = std::unique_ptr<SDCardSPI>(new SDCardSPI(spi));
-  auto status = sd_card_spi->initialize();
+
+  // Create an empty 4MB backing buffer and an iostream over it.
+  auto empty_image = std::make_unique<backing_buffer_type>(4 * 1024 * 1024, 0);
+  auto stream =
+      std::make_unique<std::basic_spanstream<char>>(std::span{*empty_image});
+
+  auto status =
+      sd_card_spi->initialize(std::move(stream), std::move(empty_image));
+  if (!status.ok()) {
+    return status;
+  }
+  return sd_card_spi;
+}
+
+absl::StatusOr<std::unique_ptr<SDCardSPI>> SDCardSPI::create(
+    SPI* spi, std::string_view image_file_name, ImageMode mode) {
+  auto file_stream = std::make_unique<std::fstream>();
+  file_stream->open(std::string(image_file_name),
+                    mode == ImageMode::kPersistedWrites
+                        ? std::ios::in | std::ios::out | std::ios::binary
+                        : std::ios::in | std::ios::binary);
+
+  if (!file_stream->is_open()) {
+    return absl::NotFoundError(
+        absl::StrCat("Failed to open file: ", std::string(image_file_name)));
+  }
+
+  std::unique_ptr<stream_type> stream;
+  std::unique_ptr<backing_buffer_type> backing_buffer;
+  switch (mode) {
+    case ImageMode::kEphemeralWrites: {
+      // Ephemeral writes means we need to load the entire file into memory.
+      backing_buffer = std::make_unique<backing_buffer_type>(
+          std::istreambuf_iterator<char>(*file_stream),
+          std::istreambuf_iterator<char>());
+      stream = std::make_unique<std::basic_spanstream<char>>(
+          std::span{*backing_buffer});
+      break;
+    }
+    case ImageMode::kPersistedWrites: {
+      // Persisted writes means we can stream the file directly.
+      stream = std::move(file_stream);
+      break;
+    }
+  }
+
+  auto sd_card_spi = std::unique_ptr<SDCardSPI>(new SDCardSPI(spi));
+  auto status =
+      sd_card_spi->initialize(std::move(stream), std::move(backing_buffer));
   if (!status.ok()) {
     return status;
   }
@@ -41,11 +110,23 @@ absl::StatusOr<SDCardSPI::Command> SDCardSPI::Command::create(
 
 SDCardSPI::SDCardSPI(SPI* spi) : spi_(spi) {}
 
-absl::Status SDCardSPI::initialize() {
+absl::Status SDCardSPI::initialize(
+    std::unique_ptr<stream_type> card_image,
+    std::unique_ptr<backing_buffer_type> backing_buffer) {
+  backing_buffer_ = std::move(backing_buffer);
+  card_image_ = std::move(card_image);
   spi_->set_byte_received_callback(
       [this](uint8_t data) { return handle_byte_in(data); });
   spi_->set_chip_select_callback(
       [this](bool enabled) { return handle_enable(enabled); });
+
+  // Compute the number of blocks in the file or image.
+  card_image_->seekg(0);
+  card_image_->ignore(std::numeric_limits<std::streamsize>::max());
+  block_count_ = card_image_->gcount() / kBlockSize;
+  // Clear the EOF flag.
+  card_image_->clear();
+  card_image_->seekg(0);
   return absl::OkStatus();
 }
 
@@ -63,6 +144,7 @@ uint8_t SDCardSPI::handle_byte_in(uint8_t data) {
       // Command byte, 4 argument bytes, and CRC byte makes 6 bytes total.
       if (input_buffer_.size() == 6) {
         auto command_or = Command::create(input_buffer_);
+        input_buffer_.clear();
         if (!command_or.ok()) {
           LOG(ERROR) << "Failed to parse command: " << command_or.status();
           card_state_ = CardState::kIdle;
@@ -80,12 +162,38 @@ uint8_t SDCardSPI::handle_byte_in(uint8_t data) {
         uint8_t response = response_queue_.front();
         response_queue_.pop();
         if (response_queue_.empty()) {
-          card_state_ = (enabled_ ? CardState::kReady : CardState::kIdle);
+          if (next_card_state_ != CardState::kUndefined) {
+            card_state_ = next_card_state_;
+            next_card_state_ = CardState::kUndefined;
+          } else {
+            card_state_ = (enabled_ ? CardState::kReady : CardState::kIdle);
+          }
         }
         VLOG(1) << "SD response byte: " << absl::Hex(response, absl::kZeroPad2);
         return response;
       }
       return 0xff;
+    case CardState::kDataToken: {
+      if (data == 0xfe) {
+        input_buffer_.clear();
+        card_state_ = CardState::kData;
+      }
+      return 0xff;
+    }
+    case CardState::kData: {
+      input_buffer_.push_back(data);
+      // We need to read 512 bytes of data plus two CRC bytes.
+      if (input_buffer_.size() == kBlockSize + 2) {
+        card_image_->seekp(write_address_ * kBlockSize);
+        card_image_->write(reinterpret_cast<const char*>(input_buffer_.data()),
+                           kBlockSize);
+        input_buffer_.clear();
+        response_queue_.push(0b0000'0101);  // Data accepted
+        response_queue_.push(0x00);  // Simulate busy for the duration of a byte
+        card_state_ = CardState::kResponse;
+      }
+      return 0xff;
+    }
     default:
       LOG(ERROR) << "Unexpected card state: " << static_cast<int>(card_state_);
       return 0;
@@ -103,8 +211,11 @@ uint8_t SDCardSPI::handle_enable(bool enabled) {
       case CardState::kReady:
         card_state_ = CardState::kCommand;
         break;
+      case CardState::kUndefined:
       case CardState::kCommand:
       case CardState::kResponse:
+      case CardState::kDataToken:
+      case CardState::kData:
         LOG(ERROR) << "Unexpected SD card state on SPI enabled: "
                    << static_cast<int>(card_state_);
         card_state_ = CardState::kCommand;
@@ -146,6 +257,57 @@ void SDCardSPI::handle_command(const Command& command) {
         response_queue_.push(0x01);
         // Check pattern echo
         response_queue_.push(command.argument & 0xff);
+        break;
+      }
+      case 17: {  // READ_SINGLE_BLOCK
+        // Set up R1
+        int32_t address = command.argument;
+        if (!ready_) {
+          response_queue_.push(kR1IllegalCommand | kR1InIdleState);
+          break;
+        } else if (address >= block_count_) {
+          response_queue_.push(kR1ParameterError);
+          break;
+        } else {
+          response_queue_.push(0);
+        }
+        // Try to read the data
+        card_image_->seekg(address * kBlockSize);
+        std::array<char, kBlockSize> block;
+        card_image_->read(block.data(), kBlockSize);
+        if (card_image_->gcount() != kBlockSize) {
+          response_queue_.push(kDataErrorTokenError);
+          // Clear any EOF we might have hit.
+          card_image_->clear();
+          break;
+        }
+        // Clear any EOF we might have hit.
+        card_image_->clear();
+        // Add a data token
+        response_queue_.push(0xfe);
+        // Then the 512 byte block
+        for (char byte : block) {
+          response_queue_.push(byte);
+        }
+        // And two CRC bytes (ignored)
+        response_queue_.push(0);
+        response_queue_.push(0);
+        break;
+      }
+      case 24: {  // WRITE_BLOCK
+        // Set up R1
+        int32_t address = command.argument;
+        if (!ready_) {
+          response_queue_.push(kR1IllegalCommand | kR1InIdleState);
+          break;
+        } else if (address >= block_count_) {
+          response_queue_.push(kR1ParameterError);
+          break;
+        } else {
+          response_queue_.push(0);
+        }
+        next_card_state_ = CardState::kDataToken;
+        write_address_ = address;
         break;
       }
       case 55: {  // APP_CMD
--- emulator/sd_card_spi.h
+++ emulator/sd_card_spi.h
@@ -1,9 +1,10 @@
 #ifndef EIGHT_BIT_SD_CARD_SPI_H
 #define EIGHT_BIT_SD_CARD_SPI_H
 
-#include <array>
+#include <istream>
 #include <memory>
 #include <queue>
+#include <string_view>
 
 #include "absl/status/status.h"
 #include "absl/status/statusor.h"
@@ -17,9 +18,27 @@ class SDCardSPI {
  public:
   ~SDCardSPI() = default;
   SDCardSPI(const SDCardSPI&) = delete;
+
+  // Create an emulated SD Card backed by 4MB of storage that's initially all
+  // zeros.
   static absl::StatusOr<std::unique_ptr<SDCardSPI>> create(SPI* spi);
 
+  enum class ImageMode {
+    // The image file is loaded from disk, but changes are in-memory only. Note:
+    // requires as much RAM as the size of the image file.
+    kEphemeralWrites,
+    // The image file is loaded from disk, and changes are written back to it.
+    kPersistedWrites,
+  };
+  // Create an emulated SD Card loaded from a file on disk. The file must exist.
+  // If mode is kPersistedWrites, changes will be written back to the file.
+  static absl::StatusOr<std::unique_ptr<SDCardSPI>> create(
+      SPI* spi, std::string_view image_file_name, ImageMode mode);
+
  private:
+  using stream_type = std::basic_iostream<char>;
+  using backing_buffer_type = std::vector<char>;
+
   struct Command {
     static absl::StatusOr<Command> create(
         const std::vector<uint8_t>& input_buffer);
@@ -29,22 +48,39 @@ class SDCardSPI {
     uint8_t crc = 0;
   };
 
-  enum class CardState { kIdle, kReady, kCommand, kResponse };
+  enum class CardState {
+    kUndefined,
+    kIdle,
+    kReady,
+    kCommand,
+    kResponse,
+    kDataToken,
+    kData
+  };
 
   SDCardSPI(SPI* spi);
-  absl::Status initialize();
+
+  absl::Status initialize(std::unique_ptr<stream_type> card_image,
+                          std::unique_ptr<backing_buffer_type> backing_buffer);
   uint8_t handle_byte_in(uint8_t data);
   uint8_t handle_enable(bool enabled);
 
   void handle_command(const Command& command);
 
   SPI* spi_;
+  // Note: card_image_ is defined after backing buffer to ensure it's deleted
+  // first
+  std::unique_ptr<backing_buffer_type> backing_buffer_;
+  std::unique_ptr<stream_type> card_image_;
+  int block_count_ = 0;
   bool enabled_ = false;
 
   // Whether or not we're considered initialized.
   bool ready_ = false;
   // Whether or not the next command is an app command.
   bool next_is_app_command_ = false;
+  // The address for the next write command.
+  uint32_t write_address_ = 0;
   // The current card state.
   CardState card_state_ = CardState::kIdle;
   // The card state we transition to after flushing the response queue.
