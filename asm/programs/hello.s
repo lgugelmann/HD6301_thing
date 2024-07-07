e07fd5e0fe2178253c1e19a6d8ae75dc634c148a
Luca Gugelmann <luca.gugelmann@gmail.com>
2024-07-07 12:37:26 +0200

    ASM: showcase the use of parameters in the 'hello' program

--- asm/programs/hello.s
+++ asm/programs/hello.s
@@ -6,11 +6,21 @@
         PUBLIC hello_start
 
 hello_string:
-        byt "Hello World!\n\0"
+        byt "Hello \0"
+world_string:
+        byt "World!\0"
 
 hello_start:
         clr GRAPHICS_CLEAR
         ldx #hello_string
+        jsr putstring
+        ; Check if a parameter was passed in. If so, print it after Hello.
+        tsx
+        ldx 2,x                 ; Get the parameter string
+        lda 0,x                 ; First char of the string
+        bne .print
+        ldx #world_string
+.print:
         jsr putstring
 .loop:
         jsr getchar
